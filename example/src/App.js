f285762845f11f3904c91327555f5d4fc094e0c7
unknown <matej.turay@gmail.com>
2019-03-20 20:33:50 +0100

    demo fix

--- example/src/App.js
+++ example/src/App.js
@@ -23,40 +23,45 @@ export default class App extends Component {
   };
   render() {
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-evenly",
-          alignItems: "center",
-          height: "100vh",
-          width: "100vw"
-        }}
-      >
-        <div>
-          <h1>Polaroid Kittens!</h1>
-          <h3>
-            React flippable polaroid component
-            <small> (kittens not included)</small>
-          </h3>
-          <h4>Click on them to see flipping in action</h4>
-        </div>
-        <div>
-          <Polaroid
-            frontText="Polaroid kitty - front with default flip"
-            rotation={0}
-            flip={this.state.flip1}
-            onClick={() => this.flip()}
-          />
-        </div>
-        <div>
-          <Polaroid
-            onClick={() => this.sideFlip()}
-            frontText="Polaroid kitty - front with side flip"
-            rotation={0}
-            flip={this.state.flip2}
-            imgSrc={"http://placekitten.com/200/300"}
-            type={"side"}
-          />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-xl-6">
+            <div>
+              <h1>Polaroid Kittens!</h1>
+              <h3>
+                React flippable polaroid component
+                <small> (kittens not included)</small>
+              </h3>
+              <h4>Click on them to see flipping in action</h4>
+            </div>
+            <div className="row" style={{ marginTop: "5rem" }}>
+              <div
+                className="col-sm-12 col-lg-3"
+                style={{ marginLeft: "5rem", marginBottom: "5rem" }}
+              >
+                <Polaroid
+                  frontText="Polaroid kitty - front with default flip"
+                  rotation={0}
+                  flip={this.state.flip1}
+                  onClick={() => this.flip()}
+                  style={{ marginRight: "2rem" }}
+                />
+              </div>
+              <div
+                className="col-sm-12 col-lg-3"
+                style={{ marginLeft: "5rem", marginBottom: "5rem" }}
+              >
+                <Polaroid
+                  onClick={() => this.sideFlip()}
+                  frontText="Polaroid kitty - front with side flip"
+                  rotation={0}
+                  flip={this.state.flip2}
+                  imgSrc={"http://placekitten.com/200/300"}
+                  type={"side"}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
