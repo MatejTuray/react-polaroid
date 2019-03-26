ccbd90d7b2578b3ba0c081f07b6898acd8b43749
MatejTuray <matej.turay@gmail.com>
2019-03-26 19:40:24 +0100

    Updates

--- precache-manifest.c87ff7025ef1be81d9723960b99679f3.js
+++ precache-manifest.c87ff7025ef1be81d9723960b99679f3.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-polaroid/static/js/runtime~main.3107b03b.js"
   },
   {
-    "revision": "884705df444088ccb0a2",
-    "url": "/react-polaroid/static/js/main.f7d3a4a7.chunk.js"
+    "revision": "7bcabe7666fc70159a66",
+    "url": "/react-polaroid/static/js/main.099939f3.chunk.js"
   },
   {
     "revision": "28f72ae3d0ee57764e14",
     "url": "/react-polaroid/static/js/2.3203889e.chunk.js"
   },
   {
-    "revision": "884705df444088ccb0a2",
+    "revision": "7bcabe7666fc70159a66",
     "url": "/react-polaroid/static/css/main.f8bb2505.chunk.css"
   },
   {
-    "revision": "e4d411601f1fbeee1bff4652bb0a873a",
+    "revision": "11650259e9472b3ad3a8cab7f87e874f",
     "url": "/react-polaroid/index.html"
   }
 ];
